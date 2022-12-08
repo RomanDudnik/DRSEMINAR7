8b4f720dbe227a37403029d56a90a7a41207203f
«Roman <research777@mail.ru>
2022-12-08 18:00:28 +0300

    DRSEMINAR7_Ex3_v2

--- Program.cs
+++ Program.cs
@@ -194,13 +194,14 @@ FindElementArray(newArray);
 //  8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+//Вариант 1 (мой)
 /*
-int [,] New2dArray (int row, int column, int minValue, int maxValue)
+int [,] New2dArray (int rows, int columns, int minValue, int maxValue)
 {
-    int [,] createdArray = new int [row, column];
+    int [,] createdArray = new int [rows, columns];
 
-    for (int i = 0; i < row; i++)
-        for (int j = 0; j < column; j++)
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
             createdArray [i,j] = new Random().Next(minValue, maxValue + 1);
     return createdArray;
 }
@@ -248,3 +249,62 @@ Console.WriteLine("Average columns is: ");
 double [] average = AverageColumns(newArray);
 */
 
+//Вариант 2 (Татьяна)
+
+int [,] New2dArray (int rows, int columns, int minValue, int maxValue)
+{
+    int [,] createdArray = new int [rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            createdArray [i,j] = new Random().Next(minValue, maxValue + 1);
+    return createdArray;
+}
+
+void Show2dArray (int [,] array)
+{
+    Console.WriteLine();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");  
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double [] AverageColumns (int [,] array1)
+{
+    double [] averArray = new double [array1.GetLength(1)];
+
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        double sum = 0;
+            for (int i =0; i < array1.GetLength(0); i++)
+                sum += array1[i, j];
+    averArray[j] = Math.Round(sum / array1.GetLength(0), 2);                          
+    }
+    return averArray;
+}
+
+void ShowArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+Console.WriteLine("Input count of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input count of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [,] new2dArray = New2dArray(m, n, min, max);
+Show2dArray(new2dArray);
+double [] avg1dArray = AverageColumns(new2dArray);
+ShowArray(avg1dArray);
